9dbca5c1b011320208c2e3be6c69eaddcd724755
Kristoff Finley <contentconsuming@gmail.com>
2020-08-31 12:25:20 -0600

    first commit

---
+++ Schedule.java
@@ -0,0 +1,16 @@
+/* *****************************************************************************
+ *  Name:    Ada Lovelace
+ *  NetID:   alovelace
+ *  Precept: P00
+ *
+ *  Description:  Prints 'Hello, World' to the terminal window.
+ *                By tradition, this is everyone's first program.
+ *                Prof. Brian Kernighan initiated this tradition in 1974.
+ *
+ **************************************************************************** */
+
+public class Schedule {
+    public static void main(String[] args) {
+        //test to see if working :)
+    }
+}
